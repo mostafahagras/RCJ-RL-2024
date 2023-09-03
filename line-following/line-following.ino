7f5bbf1518246ae0af8e8188a30b068e0f559981
mostafahagras <mostafahagras07@gmail.com>
2023-09-03 15:38:41 +0300

    Set the high_speed

--- line-following/line-following.ino
+++ line-following/line-following.ino
@@ -1,6 +1,6 @@
 // ALL VALUES ARE PLACEHOLDERS 
 #define BASE_SPEED 10
-#define HIGH_SPEED 15
+#define HIGH_SPEED 254
 #define LOW_SPEED 5
 #define SPEED_CHANGE 2
 int sensorPins[5] = { 32, 34, 36, 38, 40 }; // left2 -- left1 -- middle -- right1 -- right2
