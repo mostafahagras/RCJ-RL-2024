02082ba7f31e354098eb0bd05849aa2c71872c08
mostafahagras <mostafahagras07@gmail.com>
2024-02-05 19:44:03 +0200

    [Camera] Send to mega where to move

--- camera.py
+++ camera.py
@@ -4,13 +4,20 @@ import math
 import pyb
 from machine import UART
 
+state = 0
+search = 2
 uart = UART(3, 9600)
 uart.init(9600)
-thresholds = [(0, 20, -10, 10, -10, 10), (1, 6, -15, 12, -13, 16)] # Black
-red_led = pyb.LED(1)
-blue_led = pyb.LED(3)
-red_led.off()
-blue_led.off()
+black_thresholds = [(0, 20, -10, 10, -10, 10), (1, 6, -15, 12, -13, 16)] # Black
+triangles_thresholds = [
+    ( 0, 20, 5, 50, 5, 50 ), # RED
+    ( 10, 30, -50, 0, 5, 30 ) # GREEN
+]
+red_threshold = (0, 50, 75, 17, -30, 47)#(0, 20, 5, 50, 5, 50)
+green_threshold = (10, 30, -50, 0, 5, 30)
+#red_led = pyb.LED(1)
+#red_led.on()
+#blue_led = pyb.LED(3)
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QQVGA)
@@ -21,50 +28,70 @@ clock = time.clock()
 max_distance = 15
 max_blob_area = 5000
 center = [80, 90]
-
+RED_CODE = 1
+GREEN_CODE = 2
 def distance(x1, y1, x2, y2):
     return math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
 
 while True:
-    red_led.off()
-    blue_led.off()
     clock.tick()
-    img = sensor.snapshot().lens_corr(1.8).crop(roi=(0, 42, 160, 80))
-    blobs = img.find_blobs(thresholds, pixels_threshold=100, area_threshold=100)
-    circles = img.find_circles(
-       threshold=2500,
-       x_margin=25,
-       y_margin=25,
-       r_margin=25,
-       r_min=1,
-       r_max=30,
-       r_step=2,
-    )
-    if(len(circles) == 0): uart.write("N")
-    for c in circles:
-#        img.draw_circle(c.x(), c.y(), c.r(), color=(0,0,255))
-        isBlack = False
-        xOffset = center[0] - c.x()
-        yOffset = center[1] - c.y()
-        if(xOffset > -10 and xOffset < 10 and yOffset < 10):
-            print("Pick ball")
-        elif(xOffset > 10):
-            print("Turn left")
-        elif(xOffset < -10):
-            print("Turn right")
-        elif(yOffset > 10):
-            print("Go forward")
-#        print("x: ", xOffset,  " y: ", yOffset)
+    img = sensor.snapshot().lens_corr(1.8).crop(roi=(0, 35, 160, 80))
+    if(state % 2 or search):
+        if(state == 5 or search == 2): threshold = red_threshold
+        else: threshold = green_threshold
+        blobs = img.find_blobs([threshold], pixels_threshold=100, area_threshold=100)
         for blob in blobs:
-            d = distance(blob.cx(), blob.cy(), c.x(), c.y())
-            rect = blob.rect()
-            if(d < max_distance):
-                if(rect[2] * rect[3] < max_blob_area):
-                    isBlack = True
-                    img.draw_circle(c.x(), c.y(), c.r())
-        if(not isBlack):
-            img.draw_circle(c.x(), c.y(), c.r(), color=(255, 0, 255))
-            uart.write("S")
-        if(isBlack):
-            uart.write("B")
-#    print(clock.fps())
+            if(blob.rect()[2]/blob.rect()[3]>3):
+                if blob.elongation() > 0.5:
+                    if(state != 5):
+                        img.draw_edges(blob.min_corners(), color=(255, 0, 0))
+                        img.draw_line(blob.major_axis_line(), color=(255, 0, 0))
+                        img.draw_line(blob.minor_axis_line(), color=(255, 0, 0))
+                    else:
+                        img.draw_edges(blob.min_corners(), color=(0, 255, 0))
+                        img.draw_line(blob.major_axis_line(), color=(0, 255, 0))
+                        img.draw_line(blob.minor_axis_line(), color=(0, 255, 0))
+    else:
+        blobs = img.find_blobs(black_thresholds, pixels_threshold=100, area_threshold=100)
+        circles = img.find_circles(
+           threshold=2500,
+           x_margin=25,
+           y_margin=25,
+           r_margin=25,
+           r_min=0,
+           r_max=30,
+           r_step=2,
+        )
+        foundAnyBall = False
+        for c in circles:
+            isBlack = False
+            xOffset = center[0] - c.x()
+            yOffset = center[1] - c.y()
+            for blob in blobs:
+                d = distance(blob.cx(), blob.cy(), c.x(), c.y())
+                rect = blob.rect()
+                if(d < max_distance):
+                    if(rect[2] * rect[3] < max_blob_area):
+                        isBlack = True
+            if(not isBlack and state < 4):
+                foundAnyBall = True
+                if(xOffset > -10 and xOffset < 10 and yOffset < 10):
+                    uart.write("P")
+                elif(xOffset > 10):
+                    uart.write("L")
+                elif(xOffset < -10):
+                    uart.write("R")
+                elif(yOffset > 10):
+                    uart.write("F")
+            if(isBlack and state == 4):
+                foundAnyBall = True
+                if(xOffset > -10 and xOffset < 10 and yOffset < 10):
+                    uart.write("P")
+                elif(xOffset > 10):
+                    uart.write("L")
+                elif(xOffset < -10):
+                    uart.write("R")
+                elif(yOffset > 10):
+                    uart.write("F")
+        if(not foundAnyBall):
+            if(not search): search = 1
--- development/mega/mega.ino
+++ development/mega/mega.ino
@@ -19,7 +19,7 @@ char message[2];
 // void forwardFor(int ms) {
 //   forward();
 //   delay(ms);
-  // stop(ms);
+// stop(ms);
 // }
 
 void forward() {
@@ -33,19 +33,46 @@ void forward() {
 
 void setup() {
   Serial.begin(9600);
-  Serial2.begin(9600);
-  pinMode(LEFT_MOTOR_DIRECTION, OUTPUT);
-  pinMode(LEFT_MOTOR_PWM, OUTPUT);
-  pinMode(RIGHT_MOTOR_DIRECTION, OUTPUT);
-  pinMode(RIGHT_MOTOR_PWM, OUTPUT);
+  Serial3.begin(9600);
+  // pinMode(LEFT_MOTOR_DIRECTION, OUTPUT);
+  // pinMode(LEFT_MOTOR_PWM, OUTPUT);
+  // pinMode(RIGHT_MOTOR_DIRECTION, OUTPUT);
+  // pinMode(RIGHT_MOTOR_PWM, OUTPUT);
 }
 
 void loop() {
   // Serial.print(Serial2.available());
-  Serial2.readBytes(message, 1);
-  if(message == "R") {
-    forward();
+  Serial3.readBytes(message, 1);
+  Serial.print(message);
+  Serial.print("\t");
+  if (message[0] == 70) {
+    Serial.println("f");
+    digitalWrite(LEFT_MOTOR_DIRECTION, LEFT_MOTOR_FRONT);
+    analogWrite(LEFT_MOTOR_PWM, 40);
+    digitalWrite(RIGHT_MOTOR_DIRECTION, RIGHT_MOTOR_FRONT);
+    analogWrite(RIGHT_MOTOR_PWM, 40);
+  } else if (message[0] == 76) {
+    Serial.println("l");
+    digitalWrite(LEFT_MOTOR_DIRECTION, LEFT_MOTOR_BACK);
+    analogWrite(LEFT_MOTOR_PWM, 80);
+    digitalWrite(RIGHT_MOTOR_DIRECTION, RIGHT_MOTOR_FRONT);
+    analogWrite(RIGHT_MOTOR_PWM, 80);
+  } else if(message[0] == 82) {
+    Serial.println("r");
+    digitalWrite(LEFT_MOTOR_DIRECTION, LEFT_MOTOR_FRONT);
+    analogWrite(LEFT_MOTOR_PWM, 80);
+    digitalWrite(RIGHT_MOTOR_DIRECTION, RIGHT_MOTOR_BACK);
+    analogWrite(RIGHT_MOTOR_PWM, 80);
+  } else if(message[0] == 80) {
+    Serial.println("p");
+    analogWrite(LEFT_MOTOR_PWM, 40);
+    digitalWrite(LEFT_MOTOR_DIRECTION, LEFT_MOTOR_BACK); 
+    analogWrite(RIGHT_MOTOR_PWM, 40);
+    digitalWrite(RIGHT_MOTOR_DIRECTION, RIGHT_MOTOR_BACK); 
+  } else {
+    Serial.println("N");
+    analogWrite(LEFT_MOTOR_PWM, 0);
+    analogWrite(RIGHT_MOTOR_PWM, 0);
   }
-  Serial.println(message);
   // delay(1000);
 }
