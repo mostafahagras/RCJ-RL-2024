12099f225dcd36deb029ac99f88f5a11dcc126bf
mostafahagras <mostafahagras07@gmail.com>
2024-06-04 11:29:41 +0300

    Save analog ir threshold in eeprom

--- line-following/defines.h
+++ line-following/defines.h
@@ -1,6 +1,7 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 #include "ir.h"
+#include <EEPROM.h>
 /* Debugging, Calibration */
 #define DEBUG_MODE 0  // 0 -> off, 1 -> on
 #define CALIBRATION_PIN A13
@@ -15,7 +16,15 @@
 #define turnOffLED(p) (digitalWrite(p, LOW))
 
 /* IR Sensors */
-#define BLACK_THRESHOLD 400
+void saveToEEPROM(int value) {
+  EEPROM.write(0, (int)(value / 10));
+}
+int readFromEEPROM() {
+  byte value = EEPROM.read(0);
+  return value * 10;
+}
+int BLACK_THRESHOLD = readFromEEPROM();
+
 const uint8_t sensorPins[11] = { 53, 51, 49, 47, 45, A0, 43, 41, 39, 37, 35 };
 #define IR0 53
 #define IR1 51
@@ -28,6 +37,19 @@ const uint8_t sensorPins[11] = { 53, 51, 49, 47, 45, A0, 43, 41, 39, 37, 35 };
 #define IR8 39
 #define IR9 37
 #define IR10 35
+#define THRESHOLD_INCREMENT_INTERRUPT 18
+#define THRESHOLD_DECREMENT_INTERRUPT 19
+
+void INCREMENT_THRESHOLD() {
+  BLACK_THRESHOLD = min(BLACK_THRESHOLD + 10, 1000);
+  Serial.println(BLACK_THRESHOLD);
+  saveToEEPROM(BLACK_THRESHOLD);
+}
+void DECREMENT_THRESHOLD() {
+  BLACK_THRESHOLD = max(BLACK_THRESHOLD - 10, 20);
+  Serial.println(BLACK_THRESHOLD);
+  saveToEEPROM(BLACK_THRESHOLD);
+}
 const float weights[11] = { -0.5, -0.5, -0.5, -0.5, -0.5, 0, 0.5, 0.5, 0.5, 0.5, 0.5 };
 #define reading(a) (analogRead(a) > BLACK_THRESHOLD ? 1 : 0)
 
@@ -60,22 +82,25 @@ const float weights[11] = { -0.5, -0.5, -0.5, -0.5, -0.5, 0, 0.5, 0.5, 0.5, 0.5,
 
 /* Camera */
 extern bool dropped = true;
-extern bool inEvacuation = true;
-extern bool exitedEvacuation = false;
+extern bool inEvacuation = false;
 extern bool wallFollowing = false;
 #define RESET_EVACUATION 0
-#define PICK 0x50            // P
-#define DROP 0x44            // D
-#define LEFT 0x4C            // L (Turn left while searching for victims)
-#define FORWARD 0x46         // F (Go forward while searching for victims)
-#define RIGHT 0x52           // R (Turn right while searching for victims)
-#define U_TURN 0x55          // U (180deg turn while searching for victims)
-#define TURN_360 0x54        // T (360deg turn while searching for victims)
-#define VICTIM_LEFT 0x6C     // l (Turn left for victim)
-#define VICTIM_FORWARD 0x66  // f (Go forward to victim)
-#define VICTIM_RIGHT 0x72    // r (Turn right for victim)
-#define EXIT 0x45            // E (Exit the evacuation room)
-#define WALL_FOLLOW 0x57     // W (Wall follow to the exit)
+#define PICK 0x50               // P (Pick the victim)
+#define DROP 0x44               // D (Drop the victim)
+#define LEFT 0x4C               // L (Turn left while searching for victims)
+#define FORWARD 0x46            // F (Go forward while searching for victims)
+#define RIGHT 0x52              // R (Turn right while searching for victims)
+#define U_TURN 0x55             // U (180deg turn while searching for victims)
+#define TURN_360 0x54           // T (360deg turn while searching for victims)
+#define VICTIM_LEFT 0x6C        // l (Turn left for victim)
+#define VICTIM_FORWARD 0x66     // f (Go forward to victim)
+#define VICTIM_RIGHT 0x72       // r (Turn right for victim)
+#define WALL_FOLLOW 0x57        // W (Wall follow to the exit)
+#define START_WALL_FOLLOW 0x77  // w (Start wall following)
+#define EXIT 0x45               // E (Exit the evacuation room)
+#define SEARCH 0x53             // S (Search for victims in the evacuation room)
+#define SEARCH_TRIANGLE 0x73    // s (Search for triangles in the evacuation room)
+#define TOGGLE_EVACUATION 0x74  // t (Toggle inEvacuation)
 
 /* TOFs */
 #define RIGHT_TOF_XSHUT 6
@@ -88,8 +113,8 @@ extern bool wallFollowing = false;
 #define IMU_ERROR_FACTOR 1.2778
 
 /* Arm (Servos) */
-#define ARM 8
-#define GRIPPER 7
+#define ARM 7
+#define GRIPPER 8
 
 /* Obstacle */
 #define OBSTACLE A1
