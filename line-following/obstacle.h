39d901940daf3c0f703df6618e649d30f3a13fac
mostafahagras <mostafahagras07@gmail.com>
2024-06-11 10:06:24 +0300

    Check robot pitch before avoiding obstacle

--- line-following/obstacle.h
+++ line-following/obstacle.h
@@ -6,8 +6,25 @@
 #include "motors.h"
 
 void avoidObstacle() {
+  stop();
+  if(abs(getPitch()) > 15) return; // To stop seeing obstacle after seesaw
   chooseBus(0);
   uint16_t rightDistance = distanceRight();
+  // if(rightDistance <= 200) {
+  //   left90();
+  //   drive(254, 40);
+  //   while(_IR0() + _IR1() + _IR2() + _IR3() + _IR4() + _IR6() + _IR7() + _IR8() + _IR8() + _IR9() + _IR10() < 3) {}
+  //   forward(BASE_SPEED, 250);
+  //   left90();
+  //   backward(BASE_SPEED, 200);
+  // } else {
+  //   right90();
+  //   drive(40, 254);
+  //   while(_IR0() + _IR1() + _IR2() + _IR3() + _IR4() + _IR6() + _IR7() + _IR8() + _IR8() + _IR9() + _IR10() < 3) {}
+  //   forward(BASE_SPEED, 250);
+  //   right90();
+  //   backward(BASE_SPEED, 200);
+  // }
   if (rightDistance <= 200) {
     if (DEBUG_MODE) { Serial.println("Detected a wall on the right, turning left to avoid obstacle"); }
     left90();
@@ -64,29 +81,5 @@ void avoidObstacle() {
     }
   }
 }
-
-// void obstacle() {
-//   if (!digitalRead(OBSTACLE)) {
-//     digitalWrite(LEFT_MOTOR_DIRECTION, FRONT);
-//     digitalWrite(RIGHT_MOTOR_DIRECTION, BACK);
-//     analogWrite(LEFT_MOTOR_PWM, 127);
-//     analogWrite(RIGHT_MOTOR_PWM, 50);
-//     delay(2500);
-//     while (!digitalRead(sensorPins[2])) {
-//       digitalWrite(LEFT_MOTOR_DIRECTION, FRONT);
-//       digitalWrite(RIGHT_MOTOR_DIRECTION, FRONT);
-//       analogWrite(LEFT_MOTOR_PWM, 30);
-//       analogWrite(RIGHT_MOTOR_PWM, 127);
-//     }
-//     forward();
-//     delay(300);
-//     while (!(!(digitalRead(sensorPins[0]) || digitalRead(sensorPins[1]) || digitalRead(sensorPins[2])) && !digitalRead(sensorPins[3]) && !digitalRead(sensorPins[4]))) {
-//       digitalWrite(LEFT_MOTOR_DIRECTION, FRONT);
-//       analogWrite(LEFT_MOTOR_PWM, 150);
-//       digitalWrite(RIGHT_MOTOR_DIRECTION, BACK);
-//       analogWrite(RIGHT_MOTOR_PWM, 150);
-//     }
-//   }
-// }
 
 #endif
